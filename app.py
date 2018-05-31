405e4e0d634a6237cbb8bf216784b958bb8fb877
Gus Ostow <augustus.ostow@generalassemb.ly>
2018-05-30 19:49:07 -0700

    BUGFIX: displaying previously entered items

--- app.py
+++ app.py
@@ -9,25 +9,28 @@ from database import db_session
 app = Flask(__name__)
 app.secret_key = os.environ['APP_SECRET_KEY']
 
+
 @app.route("/", methods=('GET', 'POST'))
 def add_item():
     form = ItemForm()
     if form.validate_on_submit():
-        item = Items(name=form.name.data, quantity=form.quantity.data, description=form.description.data, date_added=datetime.datetime.now())
+        item = Items(name=form.name.data, quantity=form.quantity.data,
+                     description=form.description.data, date_added=datetime.datetime.now())
         db_session.add(item)
         db_session.commit()
         return redirect(url_for('success'))
     return render_template('index.html', form=form)
 
+
 @app.route("/success")
 def success():
     results = []
- 
+
     qry = db_session.query(Items)
     results = qry.all()
 
-    return str(results)
-  
+    return str(list(map(lambda x: x.name, results)))
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
